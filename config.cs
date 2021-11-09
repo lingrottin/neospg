6d0e35dbf248c2faf26fbf80e50b05a1ea7bd167
lingrottin <ellistwooten8269@gmail.com>
2021-11-09 22:45:42 +0800

    First Commit

---
+++ config.cs
@@ -0,0 +1,10 @@
+using System;
+namespace NeoSimplestStrongPasswordGenerator.Configuration
+{
+	public class Config
+	{
+		public static string version="0.1";
+		public static int DefaultNumber=3600;
+		public static string DefaultString="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$_&-+()/*:;!?,.={}[]~<>";
+	}
+}
