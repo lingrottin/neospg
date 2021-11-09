116351cdc6352d7cd04cb971abcf34dca33d678e
lingrottin <ellistwooten8269@gmail.com>
2021-11-09 22:49:26 +0800

    Hotfix to the default configuration

--- config.cs
+++ config.cs
@@ -3,8 +3,8 @@ namespace NeoSimplestStrongPasswordGenerator.Configuration
 {
 	public class Config
 	{
-		public static string version="0.1";
+		public static string version="NeoSPG v0.1, Made by Lingrottin & Ltfjx, Released under MIT License";
 		public static int DefaultNumber=3600;
-		public static string DefaultString="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$_&-+()/*:;!?,.={}[]~<>";
+		public static string DefaultString="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$_&-+()/*:;!?,.={}[]~<>1234567890";
 	}
 }
