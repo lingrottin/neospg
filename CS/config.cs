6916ed7eb385987765e279667228fd2c9f9b61b1
Tienyu Yang <ellistwooten8269@outlook.com>
2022-01-19 20:14:18 +0800

    Moved C# files into CS/

---
+++ CS/config.cs
@@ -0,0 +1,10 @@
+using System;
+namespace NeoSimplestStrongPasswordGenerator.Configuration
+{
+	public class Config
+	{
+		public static string version="NeoSPG v0.1, Made by Lingrottin & Ltfjx, Released under MIT License";
+		public static int DefaultNumber=3600;
+		public static string DefaultString="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$_&-+()/*:;!?,.={}[]~<>1234567890";
+	}
+}
